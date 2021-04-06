615075c7e6f6d30f394f7bea4bc5fdbc96554e32
vvladovic <vvladovic@gvc-plc.com>
2021-04-06 15:01:34 +0200

    Adding google analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue';
+import VueGtag from 'vue-gtag-next';
 import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
 
-createApp(App).use(store).mount('#app');
+const app = createApp(App);
+app.use(VueGtag, {
+  property: {
+    id: 'G-1RXZDLTVZP',
+  },
+});
+app.use(store);
+app.mount('#app');
