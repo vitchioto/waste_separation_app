ed6c739c1916e83f9ba51ad97a7078df5124725c
vvladovic <vvladovic@gvc-plc.com>
2021-04-27 23:01:51 +0200

    Updating icons

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,49 @@ module.exports = {
       swSrc: 'src/sw.js',
       swDest: 'service-worker.js',
     },
+    manifestOptions: {
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/apple-touch-icon-60x60.png',
+          sizes: '60x60',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/apple-touch-icon-76x76.png',
+          sizes: '76x76',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/apple-touch-icon-120x120.png',
+          sizes: '120x120',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/apple-touch-icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/apple-touch-icon-180x180.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/safari-pinned-tab.svg',
+          sizes: '942x942',
+          type: 'image/svg+xml',
+        },
+        {
+          src: './img/icons/msapplication-icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+      ],
+    },
   },
 };
