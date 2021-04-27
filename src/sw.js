9a0f081377f75aa491e57600222aa69eee796f64
vvladovic <vvladovic@gvc-plc.com>
2021-04-27 21:30:30 +0200

    Adding PWA configuration

---
+++ src/sw.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,14 @@ module.exports = {
   },
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ? '/wp-content/themes/wsbe/' : '/',
+  pwa: {
+    name: 'Waste Separation',
+    themeColor: '#000',
+    msTileColor: '#fff',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/sw.js',
+      swDest: 'service-worker.js',
+    },
+  },
 };
